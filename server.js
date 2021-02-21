e4f7cf8d3e96dd011e4cf4b0113d1bf75c035875
Arthur de Oliveira Batista <arthurdeol@gmail.com>
2021-02-21 11:27:35 -0300

    altering server

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var cors = require('cors');
+// var cors = require('cors');
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
+// const auth = require("json-server-auth");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 3001;
+const middlewares = jsonServer.defaults();
 
-app.db = router.db;
+// app.db = router.db;
 
-const rules = auth.rewriter({
-  "/patients*": "patients$1",
-});
+// const rules = auth.rewriter({
+//   "/patients*": "patients$1",
+// });
 
-app.use(cors());
-app.use(rules);
-app.use(auth);
+app.use(middlewares);
+// app.use(rules);
+// app.use(auth);
 app.use(router);
 app.listen(port);
 
